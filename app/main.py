4d9f6ca61f0fb32f16d5e13f55aa21b1c883e98f
santoshbarai0161 <santoshbarai0161@gmail.com>
2026-01-05 17:09:04 +0000

    Testing of post method with some data

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 app=FastAPI()
 @app.get("/")
 def root():
 	return "Hello World"
+@app.get("/posts")
+def get_posts():
+    return {"data":"Here is all of your post"}
+@app.post("/createpost")    #testing post 
+def create_post(payload:dict=Body(...)):
+    print(payload)
+    return {"new post":f"title:{payload["title"]} and content: {payload['content']}"}
